b0387533600ed0b96bded88a7275dd17a73b0ec5
wkodate <n9lfcth9@gmail.com>
2018-02-18 19:41:02 +0900

    initial commit

---
+++ src/main/java/com/wkodate/jupiter/ring/RingApplication.java
@@ -0,0 +1,20 @@
+package com.wkodate.jupiter.ring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RingApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/wkodate/jupiter/ring/RingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wkodate.jupiter.ring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
