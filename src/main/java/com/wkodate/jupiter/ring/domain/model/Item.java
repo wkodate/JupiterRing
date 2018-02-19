c714c6aff6eca13e3b0871a3395f7089cc235726
wkodate <hbwandeow@gmail.com>
2018-02-20 00:08:51 +0900

    add Item enitity

---
+++ src/main/java/com/wkodate/jupiter/ring/domain/model/Item.java
@@ -0,0 +1,77 @@
+package com.wkodate.jupiter.ring.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * Item.java
+ */
+@Entity
+@Table(name = "item")
+public class Item {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String url;
+
+    private String title;
+
+    private String description;
+
+    private Date date;
+
+    private String imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
