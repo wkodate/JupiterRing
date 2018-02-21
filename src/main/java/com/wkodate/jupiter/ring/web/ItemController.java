95c895e7092697c3b5c1c04fb1c9c31e290237a8
wkodate <hbwandeow@gmail.com>
2018-02-22 00:49:17 +0900

    use thymeleaf

--- src/main/java/com/wkodate/jupiter/ring/web/ItemController.java
+++ src/main/java/com/wkodate/jupiter/ring/web/ItemController.java
@@ -3,11 +3,15 @@ package com.wkodate.jupiter.ring.web;
 import com.wkodate.jupiter.ring.domain.Item;
 import com.wkodate.jupiter.ring.domain.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Locale;
 
 /**
  * Created by wkodate on 2018/02/19.
@@ -17,11 +21,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class ItemController {
 
     @Autowired
+    MessageSource messageSource;
+
+    @Autowired
     ItemRepository itemRepository;
 
     @RequestMapping("/hello")
-    String home() {
-        return "Hello World!";
+    String hello(Model model, Locale locale) {
+        model.addAttribute("title", messageSource.getMessage("app.title", null, locale));
+        model.addAttribute("hello", "Hello World!");
+        return "hello";
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
